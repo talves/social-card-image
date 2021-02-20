9c7a9235efadf0392189019ac9b3bcb69791584d
talves <tony@3alves.com>
2021-02-19 16:36:59 -0800

    bad function

--- netlify/functions/create-pdf.js
+++ netlify/functions/create-pdf.js
@@ -24,7 +24,6 @@ exports.handler = async function (event) {
     const context = await browser.newContext();
     const page = await context.newPage();
     const navigationPromise = page.waitForNavigation();
-    await page.emulateMediaType("screen");
     await page.goto(uri.path);
     await navigationPromise;
     await page.waitForSelector(`${uri.selector}`, { timeout: 1000 });
